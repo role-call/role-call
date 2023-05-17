72dd570843da635277fa06a0b56b1a8bbd8eddd7
Christian Klaeren <christian@baff-space.org>
2023-05-17 09:02:28 +0200

    use pytest directly

--- backend/attendance/test_superuser_views.py
+++ backend/attendance/test_superuser_views.py
@@ -2,16 +2,16 @@ import pytest
 
 from django.urls import reverse
 
-
 @pytest.mark.django_db
 def test_unauthorized(client):
-   url = reverse('superuser-url')
+   url = reverse('admin:index')
    response = client.get(url)
-   assert response.status_code == 401
+   assert (response.status_code == 302) and ( response.url == "/backend/admin/login/?next=/backend/admin/")
+
 
 
 @pytest.mark.django_db
 def test_superuser_view(admin_client):
-   url = reverse('superuser-url')
+   url = reverse('admin:index')
    response = admin_client.get(url)
    assert response.status_code == 200
